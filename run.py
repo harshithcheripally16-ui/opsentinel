5fdcd3d7979a88218572af973cbbbf818811f644
Harshith Cheripally <harshithcheripally16@gmail.com>
2026-04-05 16:36:50 +0530

    feat: Implement global preloader failsafe and force debug ignition

--- run.py
+++ run.py
@@ -2,5 +2,4 @@ import os
 from app import app
 
 if __name__ == "__main__":
-    DEBUG = os.getenv("DEBUG", "False").lower() == "true"
-    app.run(host="0.0.0.0", port=5000, debug=DEBUG)
+    app.run(host="0.0.0.0", port=5000, debug=True)
